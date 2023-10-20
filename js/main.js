8b2a46c2fdc1b9dbf65629511e607aee51d8f0a5
Fanpeng Li <serforfp@gmail.com>
2023-10-20 19:42:53 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// FAQ Accordian
+
+const cat = document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+
+  faqContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".faq-group-header");
+    console.log(groupHeader);
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    console.log(group);
+    const groupBody = group.querySelector(".faq-group-body");
+    const icon = groupHeader.querySelector("i");
+
+    // Toggle icon
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
+
+    groupBody.classList.toggle("open");
+
+    const otherGroups = faqContainer.querySelectorAll(".faq-group");
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body");
+        const otherIcon = otherGroup.querySelector(".faq-group-header i");
+
+        otherGroupBody.classList.remove("open");
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
+  });
+});
